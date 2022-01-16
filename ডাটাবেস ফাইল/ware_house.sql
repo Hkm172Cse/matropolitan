19e985bc7f48c174698d6e1ee0a85c98aeed5c22
Hkm172Cse <azizulhakim000111@gmail.com>
2022-01-17 03:18:48 +0600

    new database file uploaded

--- ডাটাবেস ফাইল/ware_house.sql
+++ ডাটাবেস ফাইল/ware_house.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2022 at 07:10 PM
+-- Generation Time: Jan 16, 2022 at 10:17 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -56,7 +56,18 @@ INSERT INTO `table_product` (`id`, `product_name`, `catagory`, `type`, `source`,
 (5, 'sope 48 gm', 'Sope', 'Promotional', 'Valuka Fectory', '0', '0000-00-00', 'image/Laravel1.png', 'Blister', 0, 120, 0, 'per box contain 120', '2021-12-15 05:23:17'),
 (6, 'Sampho', 'Promotional', 'Sample', 'Valuka Fectory', 'option2', '2021-12-15', 'image/chat22.jpg', 'Blister', 0, 24, 0, 'This is new product', '2021-12-15 05:29:57'),
 (10, '', 'Skin', 'Saleable', 'Valuka Fectory', '', '2022-01-07', 'image/bannerImg2.png', '24', 10, 18, 200, 'AVGS', '2022-01-03 18:30:04'),
-(12, 'vasiline', 'Skin Care', 'Sample', 'Valuka Fectory', 'option1', '2022-01-05', 'image/bill4.jpg', 'Piece', 15, 12, 300, 'abg', '2022-01-05 12:59:57');
+(12, 'vasiline', 'Skin Care', 'Sample', 'Valuka Fectory', 'option1', '2022-01-05', 'image/bill4.jpg', 'Piece', 15, 12, 300, 'abg', '2022-01-05 12:59:57'),
+(13, 'Kit Kat', 'Food and Beverage', 'Sample', 'Valuka Fectory', 'option1', '2022-01-16', 'image/bannerImg4.png', 'Piece', 15, 8, 96, 'VGA', '2022-01-16 15:19:35'),
+(14, 'Kit Kat', 'Food and Beverage', 'Sample', 'Valuka Fectory', 'option1', '2022-01-16', 'image/bannerImg5.png', 'Piece', 15, 8, 96, 'VGA', '2022-01-16 15:22:13'),
+(15, 'Stavary Choklet', 'Skin Care', 'Saleable', 'Valuka Fectory', 'option1', '2022-01-17', 'image/bill5.jpg', 'Sachet', 2, 20, 40, 'abc', '2022-01-16 15:26:44'),
+(16, 'bodna', 'Food and Beverage', 'Saleable', 'Valuka Fectory', 'option1', '2022-01-16', 'image/blog1.jpg', 'Dispenser', 1, 120, 120, 'ad', '2022-01-16 15:30:41'),
+(17, 'Kit Kat', 'Skin Care', 'Saleable', 'Valuka Fectory', 'option1', '2022-01-16', 'image/blog2.jpg', 'Sachet', 4, 20, 20, 'ab', '2022-01-16 15:37:09'),
+(18, 'Photato', 'Health and Hygiene', 'Promotional', 'Valuka Fectory', 'option1', '2022-01-17', 'image/bannerImg6.png', 'Piece', 25, 40, 1200, 'abc', '2022-01-16 19:49:24'),
+(19, 'Photato', 'Health and Hygiene', 'Promotional', 'Valuka Fectory', 'option1', '2022-01-17', 'image/bannerImg7.png', 'Piece', 25, 40, 1200, 'abc', '2022-01-16 19:51:58'),
+(20, 'Photato', 'Health and Hygiene', 'Promotional', 'Valuka Fectory', 'option1', '2022-01-17', 'image/bannerImg8.png', 'Piece', 25, 40, 1200, 'abc', '2022-01-16 19:52:49'),
+(21, 'Bomba Photo', 'Food and Beverage', 'Sample', 'Valuka Fectory', 'option1', '2022-01-18', 'image/bannerImg9.png', 'Packet', 10, 40, 400, 'abg', '2022-01-16 19:53:57'),
+(22, 'Chilo', 'Skin Care', 'Saleable', 'Valuka Fectory', 'option1', '2022-01-17', 'image/bill6.jpg', 'Packet', 10, 40, 400, 'VGD', '2022-01-16 19:57:26'),
+(23, 'Chips', 'Food and Beverage', 'Saleable', 'Valuka Fectory', 'option1', '2022-01-10', 'image/blog3.jpg', 'Packet', 10, 120, 240, 'abc', '2022-01-16 20:00:57');
 
 -- --------------------------------------------------------
 
@@ -69,10 +80,35 @@ CREATE TABLE `table_stock_product` (
   `product_name` varchar(255) NOT NULL,
   `quantity` double NOT NULL,
   `stock` double NOT NULL,
-  `base_unit` double NOT NULL,
+  `carton` double NOT NULL,
   `remark` double NOT NULL,
+  `aria_office` varchar(250) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `requestBy` varchar(250) NOT NULL,
+  `chalanId` varchar(50) NOT NULL,
   `created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `table_stock_product`
+--
+
+INSERT INTO `table_stock_product` (`id`, `product_name`, `quantity`, `stock`, `carton`, `remark`, `aria_office`, `date`, `requestBy`, `chalanId`, `created`) VALUES
+(56, 'vasiline', 45, 90, 12, 3.75, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-12 19:25:00'),
+(57, 'Sampho', 50, 100, 24, 2.08, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-12 19:25:00'),
+(58, 'sope 48 gm', 100, 200, 120, 0.83, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-12 19:25:00'),
+(59, 'Petrolium jely 50 gm', 200, 400, 12, 16.67, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-12 19:25:00'),
+(61, 'abc', 52, 104, 0, 0, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 15:33:04'),
+(62, 'Kit Kat', 1000, 2000, 100, 10, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 15:37:09'),
+(63, 'Kit Kat', 200, 400, 20, 10, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 16:39:03'),
+(64, 'bodna', 500, 1000, 120, 4.17, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 16:39:03'),
+(65, 'Stavary Choklet', 500, 1000, 20, 25, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 16:39:03'),
+(66, 'Kit Kat', 52, 104, 8, 6.5, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 16:39:03'),
+(67, 'Kit Kat', 200, 400, 8, 25, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 16:39:03'),
+(68, 'Photato', 150, 300, 40, 3.75, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 19:52:49'),
+(69, 'Bomba Photo', 100, 200, 25, 4, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 19:53:57'),
+(70, 'Chilo', 50, 100, 0, 0, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 19:57:26'),
+(71, 'Chips', 200, 400, 120, 1.67, 'Dhaka-1', '01/17/2022', 'Azizul Hakim', '1000795435', '2022-01-16 20:00:57');
 
 --
 -- Indexes for dumped tables
@@ -98,13 +134,13 @@ ALTER TABLE `table_stock_product`
 -- AUTO_INCREMENT for table `table_product`
 --
 ALTER TABLE `table_product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `table_stock_product`
 --
 ALTER TABLE `table_stock_product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
